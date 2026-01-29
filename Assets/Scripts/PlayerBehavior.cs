0197d92fd82dbea8056d30ffb9a3f8b6c55055e1
JustinHo09 <hojustin09@gmail.com>
2026-01-29 14:42:39 -0600

    Made the player able to hold fruit, release fruit, and changed prefab default gravity.
    We made the player able to hold on to a fruit gameobject. We also make it do that the player can drop the fruit when they press space bar. Then we disabled the gravity for the fruits prefab. Then we enabled the gravity of the fruit and set it to 1 after space bar is pressed and it released.

--- Assets/Scripts/PlayerBehavior.cs
+++ Assets/Scripts/PlayerBehavior.cs
@@ -3,6 +3,9 @@ using UnityEngine.InputSystem;
 public class PlayerBehavior : MonoBehaviour
 {
     public float speed;
+    public GameObject fruit;
+
+    public float offY = -0.6f;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
@@ -11,6 +14,26 @@ public class PlayerBehavior : MonoBehaviour
 
     // Update is called once per frame
     void Update() {
+
+        if (fruit != null) {
+            
+            //current player position
+            Vector3 playerPos = transform.position;
+            Vector3 fruitOffset = new Vector3(0.0f,offY,0.0f);
+            fruit.transform.position = playerPos + fruitOffset;
+        }
+
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            Rigidbody2D body = fruit.GetComponent<Rigidbody2D>();
+            body.gravityScale = 1.0f;
+            
+            Collider2D collider = fruit.GetComponent<Collider2D>();
+            collider.enabled = true;
+            
+            fruit = null;
+        }
+        
         if (Keyboard.current.leftArrowKey.isPressed){
             Vector3 newPos = transform.position;
             newPos.x = newPos.x - speed;
