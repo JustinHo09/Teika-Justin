cc09333a9d588fa394ff99062c6cb50f06490f53
JustinHo09 <hojustin09@gmail.com>
2026-02-17 15:13:43 -0600

    I added an OnCollisionEnter2D method to merge two matching fruits.
    So I made the OnCollisionEnter2D method to see if the colliding fruits are the same fruit. If so then it will merge the two of them prioritizing the one at the lower positino to create the fruit. Then it creates a fruit in between the two other ones, and then destroys those two.

--- Assets/Scripts/FruitBehavior.cs
+++ Assets/Scripts/FruitBehavior.cs
@@ -2,18 +2,45 @@ using UnityEngine;
 
 public class FruitBehavior : MonoBehaviour
 {
+	public GameObject[] fruits;
+	public int fruitType;
 
     //public float timeOut;
     //public float timeStart;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start() {
-        
+        fruits = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerBehavior>().fruits;
     }
 
     // Update is called once per frame
     void Update() {
         
     }
+
+
+	private void OnCollisionEnter2D (Collision2D other){
+		if(other.gameObject.CompareTag("Fruit"))
+		{
+			int otherType = other.gameObject.GetComponent<FruitBehavior>().fruitType;
+			if(otherType == fruitType && fruitType < fruits.Length-1) {
+				if (gameObject.transform.position.y < other.transform.position.y ||
+				    (gameObject.transform.position.y == other.transform.position.y &&
+				     gameObject.transform.position.x < other.transform.position.x)) {
+					// create the fruit
+					int choice = fruitType + 1;
+					GameObject currentFruit = Instantiate(fruits[choice], Vector3.Lerp(
+							gameObject.transform.position, other.gameObject.transform.position, 0.5f),
+						Quaternion.identity);
+					currentFruit.GetComponent<Collider2D>().enabled = true;
+					currentFruit.GetComponent<Rigidbody2D>().gravityScale = 1.0f;
+
+					Destroy(other.gameObject);
+					Destroy(gameObject);
+				}
+
+			}
+		}
+	}
 
     // private void OnTriggerEnter2D(Collider2D other) {
     //     if (other.gameObject.CompareTag("TopBorder")) {
